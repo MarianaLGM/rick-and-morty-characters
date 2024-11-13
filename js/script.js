af9bd857e710d0a41cf86c8bf3aaeea5f76a380b
MarianaLGM <mariana_lobeto@hotmail.com>
2024-11-13 11:23:19 +0100

    add index.html

--- js/script.js
+++ js/script.js
@@ -1,40 +1,35 @@
 //Aquí tu código
 
-
-
 function rickAndMorty (pagina){
 const characterList=document.getElementById ("character-list");
 characterList.innerHTML=""
 fetch("https://rickandmortyapi.com/api/character/?page="+ pagina)
-  .then((response)=>response.json())
+  .then((response)=>response.json()) //respuesta del servidor 
   .then((data)=>{
     console.log(data)
     console.log(data.results)
-
+    
     data.results.forEach((elements) => {
-      const nuevoContenedorJs =document.createElement("div") //creo un nuevo contenedor donde voy a meter todos esos datos api para que vuelquen en web
+      const nuevoContenedorJs =document.createElement("div") //creo un nuevo contenedor donde voy a meter todos esos datos api 
       nuevoContenedorJs.classList.add ("nuevoDiv")
+      //añado imagen, name y species
       nuevoContenedorJs.innerHTML= `  
        <img src="${elements.image}">
        <h4><span class="bold">Name:</span> ${elements.name}</h4>
        <h4><span class="bold">Species:</span>${elements.species}</h4>
      `; 
       characterList.appendChild(nuevoContenedorJs);
-
     });
-
   });
-
 }
+
+
 
 //paginación
 const prevPageButton=document.getElementById ("prev-page");
 const nextPageButton=document.getElementById ("next-page");
 
-const elementosPorPagina = 20;
 let paginaActual=0;
-
-
 
 	// Incrementar "paginaActual"
 function avanzarPagina() {
@@ -54,11 +49,7 @@ function retrocederPagina() {   // Disminuye "paginaActual"
   prevPageButton.addEventListener("click", retrocederPagina);
   nextPageButton.addEventListener("click", avanzarPagina);
 
-  
+  //.catch (error) maneja el error
+  //.finaly
 
-    
-
-
-
-
-    
+  rickAndMorty(paginaActual) //llamar a la funcion para que me vuelque todo
